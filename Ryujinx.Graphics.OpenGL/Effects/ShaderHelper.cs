48522f96522abe67d2dc53de8a21fc16cd1a65d1
s32303 <111225566+s32303@users.noreply.github.com>
2022-10-30 07:09:59 +0800

    Pr 01 (#1)
    * Add short duration texture cache
    
    This texture cache takes textures that lose their last pool reference and keeps them alive until the next frame, or until an incompatible overlap removes it. This is done since under certain circumstances, a texture's reference can be wiped from a pool despite it still being in use - though typically the reference will return when rendering the next frame.
    
    While this may slightly increase texture memory usage when quickly going through a bunch of temporary textures, it's still bounded due to the overlap removal rule.
    
    This greatly increases performance in Hyrule Warriors: Age of Calamity. It may positively affect some UE4 games which dip framerate severely under certain circumstances.
    
    * Small optimization
    
    * Don't forget this.
    
    * Add short cache dictionary
    
    * Address feedback
    
    * Add Post processing effects
    
    * fix typo
    
    * move shaders to embedded files
    
    * rename fxaa files
    
    * Initial implementation
    
    * Some improvements.
    
    * Fix incorrect cast
    
    * Performance improvement and improved correctness
    
    * Add very fast path when all handles are checked.
    
    * Slightly faster
    
    * Add comment
    
    * De-virtualize region handle
    
    All region handles are now bitmap backed.
    
    * Remove non-bitmap tracking
    
    * Remove unused methods
    
    * Add docs, remove unused methods
    
    * Address Feedback
    
    * Rename file
    
    Co-authored-by: riperiperi <rhy3756547@hotmail.com>
    Co-authored-by: Emmanuel Hansen <emmausssss@gmail.com>

---
+++ Ryujinx.Graphics.OpenGL/Effects/ShaderHelper.cs
@@ -0,0 +1,44 @@
+﻿using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ryujinx.Graphics.OpenGL.Effects
+{
+    internal static class ShaderHelper
+    {
+        public static int CompileProgram(string shaderCode, ShaderType shaderType)
+        {
+            var shader = GL.CreateShader(shaderType);
+            GL.ShaderSource(shader, shaderCode);
+            GL.CompileShader(shader);
+
+            var program = GL.CreateProgram();
+            GL.AttachShader(program, shader);
+            GL.LinkProgram(program);
+
+            GL.DetachShader(program, shader);
+            GL.DeleteShader(shader);
+
+            return program;
+        }
+
+        public unsafe static int CompileProgram(string[] shaders, ShaderType shaderType)
+        {
+            var shader = GL.CreateShader(shaderType);
+            GL.ShaderSource(shader, 2, shaders, (int*)IntPtr.Zero);
+            GL.CompileShader(shader);
+
+            var program = GL.CreateProgram();
+            GL.AttachShader(program, shader);
+            GL.LinkProgram(program);
+
+            GL.DetachShader(program, shader);
+            GL.DeleteShader(shader);
+
+            return program;
+        }
+    }
+}
